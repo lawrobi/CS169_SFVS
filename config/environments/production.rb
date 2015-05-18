79dc52c21196e0a01607a8ca6eb70728540a11c6
Christopher Coale <ccoale@berkeley.edu>
2015-05-17 22:35:16 -0300

    fixed email url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,14 +70,14 @@ SFVSRegistrationSystem::Application.configure do
   config.force_ssl = true
 
   # Devise Mailer
-  config.action_mailer.default_url_options = {:host => 'sfvsapp.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => 'sfvs-wvd.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'sfvsapp.herokuapp.com',
+    :domain         => 'sfvs-wvd.herokuapp.com',
     :authentication => :plain
   }
 end
